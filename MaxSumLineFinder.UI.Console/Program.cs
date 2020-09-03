b6f882d1a5ac7a06746359b96731c827bc3c393e
Oleksandr Desiatnyk <korrsfeddel@gmail.com>
2020-09-03 21:12:45 +0300

    init repo, solution structure

---
+++ MaxSumLineFinder.UI.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MaxSumLineFinder.UI.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+        }
+    }
+}
