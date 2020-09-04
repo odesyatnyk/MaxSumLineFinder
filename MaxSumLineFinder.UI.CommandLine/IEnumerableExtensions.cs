504b7f366171e92fdbfbd036632b454d11d89023
Oleksandr Desiatnyk <korrsfeddel@gmail.com>
2020-09-04 10:21:47 +0300

    implement console UI

---
+++ MaxSumLineFinder.UI.CommandLine/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaxSumLineFinder.UI.CommandLine
+{
+    public static class IEnumerableExtensions
+    {
+        public static string ToCommaSeparatedString<T>(this IEnumerable<T> source)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.Aggregate(string.Empty, (current, next) => string.Concat(current, string.IsNullOrWhiteSpace(current) ? string.Empty : ", ", next.ToString()));
+    }
+    }
+}
---
+++ MaxSumLineFinder.UI.CommandLine/Program.cs
@@ -0,0 +1,73 @@
+﻿using MaxSumLineFinder.ApplicationLogic;
+using System;
+using System.Linq;
+
+namespace MaxSumLineFinder.UI.CommandLine
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string path;
+
+            if (args.Length == 0)
+            {
+                Console.Write("Please enter a path string: ");
+                path = Console.ReadLine();
+            }
+            else
+            {
+                path = args[0];
+            }
+
+            var parser = new FileParser();
+
+            try
+            {
+                var result = parser.Parse(path);
+                DisplayResult(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured during current operation:");
+                Console.WriteLine($"  - {ex.Message}");
+                Console.WriteLine("Stack Trace");
+                Console.WriteLine($"{ex.StackTrace}");
+
+                if (!(ex.InnerException is null))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Inner exception:");
+                    Console.WriteLine($"  - {ex.InnerException.Message}");
+                    Console.WriteLine("Inner exception Stack Trace:");
+                    Console.WriteLine($"{ex.InnerException.StackTrace}");
+                }
+                
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to exit");
+                Console.ReadLine();
+                return 1;
+            }
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+            return 0;
+        }
+
+        static void DisplayResult(FileParseResult result)
+        {
+            if (!result.AnyInvalidLine && !result.AnyValidLine)
+            {
+                Console.WriteLine("No results to display.");
+            }
+            else
+            {
+                if (result.AnyValidLine)
+                    Console.WriteLine($"Lines with max sum of elements: {result.LinesWithMaxSum.ToCommaSeparatedString()}");
+
+                if (result.AnyInvalidLine)
+                    Console.WriteLine($"Invalid lines: {result.InvalidLines.ToCommaSeparatedString()}");
+            }
+        }
+    }
+}
